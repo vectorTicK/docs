8b16f8f4332b103e9aca26b10eee75ea6e8bb77b
tick <tk1011@qq.com>
2020-12-14 16:30:12 +0800

    初始化项目、完成部署

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    base:"/docs/",
+    title: '二羊话叹',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Ubuntu', link: '/ubuntu/' },
+          { text: 'External', link: 'https://bing.com' },
+        ]
+      }
+  }
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: /assets/img/logo.jpg
+heroText: 二羊话叹
+tagline: 没事儿学习学习ubuntu、学习学习web
+actionText: 正在学习 →
+actionLink: /ubuntu/
+footer: Good Good Study Day Day Up
+---
+
